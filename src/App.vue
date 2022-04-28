4b15918e3aa1a45ab35686953bfbb108c70033fd
Esther <46933039+estherenrdi@users.noreply.github.com>
2022-04-28 12:36:41 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,20 @@ export default {
   data() {
     return {
       titleClass: 'title'
+      text: ''
     }
   }
 }
 </script>
 
 <template>
-  <h1 :class="titleClass">Make me red</h1>
+  <h1 :class="titleClass">Massive Data project</h1>
+  <h2> This is a project developed by miriam, clau, esther,ire, and elena </h2>
+  <input v-model="text" placeholder="Type here">
+  <p>{{ text }}</p>
 </template>
 
 <style>
 .title {
-  color: red;
+  color: blue;
 }
-</style>
