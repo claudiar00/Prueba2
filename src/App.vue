c81d6b605e910a08c812478511e99344d4492faf
Esther <46933039+estherenrdi@users.noreply.github.com>
2022-04-28 12:23:02 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      titleClass: 'title'
+    }
+  }
+}
+</script>
+
 <template>
-  <h1>Hello World!</h1>
+  <h1 :class="titleClass">Make me red</h1>
 </template>
+
+<style>
+.title {
+  color: red;
+}
+</style>
