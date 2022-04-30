faa6255901954ce4937c6f6d72995b2ad58137c6
claudiar00 <46930415+claudiar00@users.noreply.github.com>
2022-04-30 22:34:20 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
-
-<script setup>
+<script>
+export default { data() {return {message: ''}}
 import { ref } from 'vue'
 
 const message = ref('Welcome to Global Terrorism Database!')
+const intro = ('Please select a country to check their terrorism statistics:')
+const selected = ref('')
 </script>
 
 <template>
   <h1>{{ message }}</h1>
+  <h5>{{intro}}</h5>
+  <select v-model="Selected">
+    <option disabled value="">Please select one</option>
+    <option>Iraq</option>
+    <option>India</option>
+    <option>Spain</option>
+  </select>
+  <br>
+  <br>
+  <span>The country selected is: {{ selected }}</span>
 </template>
+
+
