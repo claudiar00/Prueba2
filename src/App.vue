073415e9dae3171f391ff1884f89448278bf1a08
claudiar00 <46930415+claudiar00@users.noreply.github.com>
2022-04-30 21:54:30 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -20,9 +20,7 @@ export default {
 
 <template>
   <button @click="count++">Count is: {{ count }}</button>
-</template>
 
-<template>
 	<p>Message is: {Welcome to Global Terrorism Database} </p>
 	<input v-model="message" placeholder="edíteme">
 	<p>El mensaje es: {{ message }}</p>
